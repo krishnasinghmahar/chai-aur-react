8c66dc0c4d3dec33a62e8dd3034a5f01828b37cc
krishna singh <krishnasinghmaharksm@gmail.com>
2024-12-29 12:46:40 +0530

    commit

--- 07reactRouter/src/components/index/index.js
+++
@@ -1,8 +0,0 @@
-import Home from "../Home/Home";
-import Header from "../Header/Header";
-import Contact from "../Contact/Contact";
-import Footer from "../Footer/Footer";
-import About from "../About/About"
-
-
-export { Home, Header, Contact, Footer, About }
