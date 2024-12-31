92d79e9ecca8e82ae8d6be870b96e24294007fec
krishna singh <krishnasinghmaharksm@gmail.com>
2024-12-31 18:43:24 +0530

    add Tailwind CSS setup with Vite and create Card component

---
+++ 03-tailwindProps/src/App.jsx
@@ -0,0 +1,12 @@
+import Card from '../components/Card';
+
+function App() {
+  return (
+    <>
+    <Card heading='Noteworthy technology 2021' btnText='click Me' />
+    <Card heading='Noteworthy technology 2022' btnText='Read Me' />
+    </>
+  )
+}
+
+export default App
